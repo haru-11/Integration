3d7916f3658a7eef0ece8b7e0b9ad2dc397fff20
“haru-11” <degmyb.5534g@gmail.com>
2018-08-31 16:55:57 +0000

    0901

--- gps_2.py
+++ gps_2.py
@@ -1,10 +1,16 @@
 import serial
-
-s = serial.Serial('/dev/serial0', 115200, timeout=10)
+import time
+s = serial.Serial('/dev/serial0', 38400, timeout=10)
 
 s.readline()
-
+time.sleep(1.0)
+#s.close()
+time.sleep(1.0)
 while True:
-
+	#s = serial.Serial('/dev/serial0', 38400, timeout=10)
 	sentence = s.readline()
 	print(sentence)
+	print(type(sentence))
+	time.sleep(0.2)
+	#s.close()
+	time.sleep(0.2)
